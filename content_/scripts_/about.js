2af4f23c7261c6dd7e3c01b63f31ed22fec32be0
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-19 01:31:15 -0300

    v2.4

--- content_/scripts_/about.js
+++ content_/scripts_/about.js
@@ -1,6 +1,17 @@
 const profilePhoto = document.querySelector('#profilePhoto');
 
+const closeModal = document.querySelector('#closeModal');
+const spanImgProfile = document.querySelector('#spanImgProfile');
+
+closeModal.addEventListener('click', () => {
+
+    spanImgProfile.style.display = 'none';
+    profilePhoto.style.transform = 'scale(1.0)';
+})
+
 profilePhoto.addEventListener('click', () => {
 
-    profilePhoto.classList.toggle('activeProfile');
+    profilePhoto.style.transform = 'scale(1.2)';
+    spanImgProfile.style.display = 'block';
+
 })
